217b18da01dec0fe3e4130ecf02db2728b5daf61
fodity <iva55rib@gmail.com>
2026-09-21 13:08:43 +0000

    Lab2: implement Swap and RevertString

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,14 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int str_len =  strlen(str);
+    for (int i = 0; i < str_len / 2; i++){
+        int j = str_len - 1 - i;
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
 }
 
